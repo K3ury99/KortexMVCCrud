56cdf5a1c34aedd807c25950c01383d7fa7a4f69
K3ury <keury2004@hotmail.com>
2024-04-09 00:08:40 -0400

    Migraciones hechas

--- KortexCrud/obj/Debug/net8.0/KortexCrud.AssemblyInfo.cs
+++ KortexCrud/obj/Debug/net8.0/KortexCrud.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KortexCrud")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8fcda46a476f0a9d7978ecafa96fb1eb552ed611")]
 [assembly: System.Reflection.AssemblyProductAttribute("KortexCrud")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KortexCrud")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
