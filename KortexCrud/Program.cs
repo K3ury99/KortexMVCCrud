bf79d26337ecd4b3f4f8bf7231b50e2d65cf4ecc
Keury Ramírez <128008789+K3ury99@users.noreply.github.com>
2024-04-10 15:44:20 -0400

    Update Program.cs

--- KortexCrud/Program.cs
+++ KortexCrud/Program.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using System.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
-
+ 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
